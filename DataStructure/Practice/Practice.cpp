410b6af0be945265f586cd7233b879cd0585375a
iwryu <dlfdnd96@gmail.com>
2020-09-06 11:07:58 +0900

    Java 프로젝트 추가

--- DataStructure/Practice/Practice.cpp
+++ DataStructure/Practice/Practice.cpp
@@ -1,24 +1,72 @@
 #include <iostream>
-#include <exception>
-#include <vector>
 
 using namespace std;
 
+struct Calendar
+{
+	unsigned int mYear;
+	unsigned int mMonth;
+	unsigned int mDay;
+};
+
+istream& operator>>(istream& is, Calendar& t)
+{
+	cin >> t.mYear;
+	is.ignore(1, '.');
+	cin >> t.mMonth;
+	is.ignore(1, ':');
+	cin >> t.mDay;
+
+	return is;
+}
+ostream& operator<<(ostream& os, Calendar& t)
+{
+	if (t.mDay < 10)
+	{
+		cout << "0" << t.mDay << "-";
+	}
+	else
+	{
+		cout << t.mDay << "-";
+	}
+
+	if (t.mMonth < 10)
+	{
+		cout << "0" << t.mMonth << "-";
+	}
+	else
+	{
+		cout << t.mMonth << "-";
+	}
+
+	if (t.mYear < 1000)
+	{
+		if (99 < t.mYear < 1000)
+		{
+			cout << "0" << t.mYear << endl;
+		}
+		else if (9 < t.mYear < 100)
+		{
+			cout << "00" << t.mYear << endl;
+		}
+		else if (0 < t.mYear < 10)
+		{
+			cout << "000" << t.mYear << endl;
+		}
+	}
+	else
+	{
+		cout << t.mYear << endl;
+	}
+
+	return os;
+}
+
 int main()
 {
-	vector<__int32>* temp = new vector<__int32>(3);
-	temp->at(0) = 1;
-	temp->at(1) = 2;
-	temp->at(2) = 3;
-	//temp->at(3) = 4;
-	temp[0][0] = 4;
-	temp[0][1] = 5;
-	temp[0][2] = 6;
-
-	for (const auto& v : *temp)
-	{
-		cout << v << endl;
-	}
+	struct Calendar t;
+	cin >> t;
+	cout << t;
 
 	return 0;
 }
