108747b4966a400a330fe3e5cff54da26be67fb5
iwryu <dlfdnd96@gmail.com>
2020-09-17 23:04:57 +0900

    알고리즘 공부중

--- DataStructure/FindDependencyDataSpecifiedInGraph/FindDependencyDataSpecifiedInGraph.cpp
+++ DataStructure/FindDependencyDataSpecifiedInGraph/FindDependencyDataSpecifiedInGraph.cpp
@@ -19,20 +19,26 @@ using std::string;
 class Project
 {
 public:
-    Project(string name) : mName(name), mMarked(false) {}
+    Project();
+    Project(string name) : mName(name), mMarked(false)
+    {
+        this->mDependencies = new vector<Project>();
+    }
     string GetName() const;
     void SetName(string const name);
-    vector<Project> GetDependencies() const;
+    vector<Project>* GetDependencies() const;
     bool GetMarked() const;
-    void SetMarked(bool const mark);
-    void AddDependency(Project& project);
+    constexpr void SetMarked(bool const mark);
+    void AddProjectDependency(const Project* project) const;
+    vector<Project>* GetProjectDependencies() const;
 
 private:
     string mName;
-    vector<Project> mDependencies;
+    vector<Project>* mDependencies;
     bool mMarked;
 }; // end Project class
 
+/* Getter, Setter */
 string Project::GetName() const
 {
     return mName;
@@ -41,7 +47,7 @@ void Project::SetName(string const name)
 {
     this->mName = name;
 }
-vector<Project> Project::GetDependencies() const
+vector<Project>* Project::GetDependencies() const
 {
     return this->mDependencies;
 }
@@ -49,55 +55,128 @@ bool Project::GetMarked() const
 {
     return this->mMarked;
 }
-void Project::SetMarked(bool const mark)
+constexpr void Project::SetMarked(bool const mark)
 {
     this->mMarked = mark;
 }
 
-void Project::AddDependency(Project& project)
+/* function */
+void Project::AddProjectDependency(const Project* project) const
 {
-    vector<Project>::iterator it = std::find(mDependencies.begin(), mDependencies.end(), project);
+    vector<Project>::iterator iter = std::find(mDependencies->begin(), mDependencies->end(), project);
     
-    if (it == mDependencies.end())
+    if (iter == mDependencies->end())
     {
-        mDependencies.push_back(project);
+        mDependencies->emplace_back(*project);
     }
+}
+
+vector<Project>* Project::GetProjectDependencies() const
+{
+    return this->mDependencies;
 }
 
 class ProjectManager
 {
 public:
-    ProjectManager(string* const names, string** const dependencies)
+    ProjectManager(const vector<string>& names, const vector<vector<string>>& dependencies)
     {
-        BuildProjects(names);
-        AddDependencies(dependencies);
+        buildProjects(&names);
+        addDependencies(dependencies);
     }
-    void BuildProjects(string* const names);
-    void AddDependencies(string** dependencies);
+    vector<Project>* BuildOrder();
 
 private:
-    map<string, Project> projects;
+    void buildProjects(const vector<string>* const names);
+    void addDependencies(const vector<vector<string>>& dependencies);
+    Project& findProject(const string dependency) const;
+    void buildOrderRecursive(Project& project, vector<Project>* ordered);
+    void initMarkingFlags() const;
+
+private:
+    map<string, Project>* projects;
+    int index;
 }; // end ProjectManager class
 
-void ProjectManager::BuildProjects(string* const names)
+/* function */
+vector<Project>* ProjectManager::BuildOrder()
 {
-    for (unsigned __int32 index = 0; index < names->length(); ++index)
+    initMarkingFlags();
+
+    vector<Project>* ordered = new vector<Project>(this->projects->size());
+    this->index = 0;
+    for (map<string, Project>::iterator iter = this->projects->begin(); iter != this->projects->end(); ++iter)
     {
-        projects.insert(std::pair<string, Project>(names[index], Project(names[index])));
+        buildOrderRecursive(iter->second, ordered);
+    }
+
+    return ordered;
+}
+
+void ProjectManager::buildProjects(const vector<string>* const names)
+{
+    this->projects = new map<string, Project>();
+    for (unsigned __int32 index = 0; index < names->size(); ++index)
+    {
+        projects->insert(std::pair<string, Project>(names[0][index], Project(names[0][index])));
     }
 }
 
-void ProjectManager::AddDependencies(string** dependencies)
+void ProjectManager::addDependencies(const vector<vector<string>>& dependencies)
 {
-    for (string* dependency : dependencies)
+    for (vector<vector<string>>::iterator::value_type dependency : dependencies)
     {
-        Project before = FindProject(dependency[0]);
-        Project after = findProject(dependency[1]);
-        after.AddDependency(before);
+        const Project before = findProject(dependency[0]);
+        const Project after = findProject(dependency[1]);
+        after.AddProjectDependency(&before);
     }
+}
+
+void ProjectManager::buildOrderRecursive(Project& project, vector<Project>* ordered)
+{
+    if (!project.GetDependencies()->empty())
+    {
+        for (Project p : *project.GetDependencies())
+        {
+            buildOrderRecursive(p, ordered);
+        }
+    }
+
+    if (!project.GetMarked())
+    {
+        project.SetMarked(true);
+        (*ordered)[this->index] = project;
+        ++this->index;
+    }
+}
+
+void ProjectManager::initMarkingFlags() const
+{
+    for (map<string, Project>::iterator iter = this->projects->begin(); iter != this->projects->end(); ++iter)
+    {
+        iter->second.SetMarked(false);
+    }
+}
+
+Project& ProjectManager::findProject(const string name) const
+{
+    map<string, Project>::iterator iter = find(this->projects->begin(), this->projects->end(), name);
+    return iter->second;
 }
 
 int main()
 {
+    vector<string> projects = { "a", "b", "c", "d", "e", "f", "g" };
+    vector<vector<string>> dependencies = { {"f", "a"}, {"f", "b"}, {"f", "c"}, {"b", "a"}, {"c", "a"}, {"a", "e"}, {"b", "e"}, {"d", "g"} };
+    ProjectManager* pm = new ProjectManager(projects, dependencies);
+    vector<Project>* ps = pm->BuildOrder();
+    for (int index = 0; index < ps->size(); ++index)
+    {
+        if (!ps[index].empty())
+        {
+            cout << (*ps)[index].GetName() + " ";
+        }
+    }
+    
     return 0;
 }
